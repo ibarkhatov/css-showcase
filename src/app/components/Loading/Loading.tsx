fd87277030d37819c88e58f5e0b0abd1abf6daa4
Igor Barkhatov <ibarhatov@cart.is>
2023-06-12 20:58:57 +0400

    add neon loading animation

---
+++ src/app/components/Loading/Loading.tsx
@@ -0,0 +1,27 @@
+import styles from "./loading.module.css";
+import { Property } from "csstype";
+
+const Loading = () => {
+  const NUMBER_OF_DOTS = 20;
+  const dotsNumberList = [...Array(NUMBER_OF_DOTS).keys()];
+
+  return (
+    <section className={styles.loaderSection}>
+      <div className={styles.loaderContainer}>
+        {dotsNumberList.map((number) => (
+          <span
+            key={number}
+            className={styles.loaderDot}
+            style={
+              {
+                "--i": number,
+              } as React.CSSProperties
+            }
+          ></span>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
